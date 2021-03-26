249eef3e407fc1bd8ef71a5732cca104ed05dc14
syamil437 <72728501+syamil437@users.noreply.github.com>
2021-03-26 15:44:38 +0800

    Standardized OB and OS significant level for semiconductor sector

---
+++ CombineOB&OS/credentials.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+login = {
+    "username_db" : "admin",
+    "password_db" : "isahamyousahamwesaham",
+    "ip_db" : "139.59.227.18",
+    "port_db" : "27017",
+
+	"key_isaham" : "2I49r43f44a41n26 66N81o09o21r36d31i08n24"
+	
+	
+}
+
+
+# In[ ]:
+
+
+
+
